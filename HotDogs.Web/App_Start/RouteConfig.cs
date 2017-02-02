0088677ab3e4bff7fe3c5c915f08d9706ec921ff
Laurent Cudini <lcudini@klanik.com>
2017-02-02 20:11:40 +0100

    Fix route par defaut

--- HotDogs.Web/App_Start/RouteConfig.cs
+++ HotDogs.Web/App_Start/RouteConfig.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 using System.Web.Routing;
 
 namespace HotDogs.Web
@@ -16,7 +12,7 @@ namespace HotDogs.Web
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { action = "Index", id = UrlParameter.Optional }
+                defaults: new { Controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
